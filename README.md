555bc6dc741b4f3e8bfb93b1a75222ffac7bd9f5
admin <admin@e240-nb19.inet.dkfz-heidelberg.de>
2021-06-29 23:19:26 +0200

    Initial commit of scoring scheme

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Simple.-Variant-Ranking-Annotation-CAncer-Score
-A simple cancer variant prioritization score that can be used as an extension to open-source OpenCRAVAT toolkit at a single patient resolution
+# Scoring initial theme
+
+### Efstathios-Iason Vlachavas
+#### DKFZ-Department of Molecular Genome Analysis
+##### Efstathios-Iason.Vlachavas@dkfz-heidelberg.de
+
+## Description
+
+The main goal of the developing a ranking scheme in translational cancer research, is to aid the biological interpretation of lists of annotated cancer variants at the single patient resolution. Briefly, taking into account different annotation resources, such as variant effect prediction, cancer evidence, expression and systems biology properties, an integrated scoring value is assigned to each variant as a *hollistic* score, ranking variants from a single patient mutations list. In addition, the first version of the scoring process is based on single nucleotide changes that occur in the protein-coding space; that is, somatic mutations that can lead to several possible changes to a protein. Overall, the main rationale is despite the fact that a significant number of variant annotation tools in cancer research are available for a robust exploitation of putative somatic variants, no direct score or assessment is available for a simple ranking or prioritization of the interrogated variants, based on the plethora of distinct evidence. 
+
+The ranking score is based on the output of the OpenCRAVAT annotation platform (https://doi.org/10.1200/cci.19.00132) and can serve as an additional *plug-in*, aiding in the interpretation of the annotated variant calling results. The open-source OpenCRAVAT toolkit possesses significant and important features, by making it possible to integrate multiple sources of evidence for variant annotation and exploitation.
+
+In addition, for the construction and the relative score cut-offs, various guidelines and publications were considered for estimating the oncogenicity of somatic mutations, based on big consortia, such as:
+
+1. [Belgian ComPerMed Expert Panel](https://doi.org/10.3390/cancers11122030) 
+
+2. [Variant Interpretation for Cancer Consortium (VICC)](https://cancervariants.org/research/standards/onc_path_sop/) 
+
+
+## Implementation
+
+1. The user has to initially run OpenCRAVAT web server (https://run.opencravat.org/) or install locally (https://open-cravat.readthedocs.io/en/latest/quickstart.html). The input can be a vcf file, or a txt with necessary columns (https://open-cravat.readthedocs.io/en/latest/File-Formats.html)
+
+2. The following annotators should run: gnomAD, ClinVar, CIViC, Mutation Assessor, FATHMM XF Coding, VEST4, SpliceAI, COSMIC, CScape Coding, Cancer Gene Census,
+Cancer Gene Landscape, Cancer Hotspots, SiPhy and Phast Cons (15 annotators if having hg19 as the reference genome, to also include **hg19 coordinates**).
+
+3. Next, an RData file has to be created either from the download section of the web server, or locally using the installed version of OpenCRAVAT
+
+```python
+oc report example_input.sqlite -t rdata
+
+```
+
+For more details see [here](https://open-cravat.readthedocs.io/en/latest/Reporter.html#example)
+
+4. Finally, after creating the necessary RData file including the variants from one patient/sample, the main function in R to run:
+
+```r
+ranked_snvs = predict_sysSVM2(rdata_dir,exp.genes=NULL)
+
+```
+
+## Dependencies
+
+```r
+install.packages(c("DT","tidyverse","jsonlite"))
+
+```
+
+## Reproducible example
+
+Here we present a simple example using the mutations from a randomly selected colorectal cancer patient sample ("crc4") from published Reiter et al., 2018 study (https://doi.org/10.1126/science.aat7171), mainly utilizing the Kim et al., 2015 publication (https://clincancerres.aacrjournals.org/content/21/19/4461#:~:text=10.1158/1078-0432.CCR-14-2413). Then, the web version of OpenCRAVAT was used to perform integrative variant annotation using the 15 aforementioned annotators, and the relative RData file was created. Below, a snapshot of the created html file with the top 10 hits are depicted:
+
+![Alt text](relative/path/to/img.jpg?raw=true "Top 10 ranked variants example")
+
+## Utilization feedback
+
+For any questions, suggestions or issues please directly use my email or the github issue page 
+
+## Acknowledgements
+
+Stefan Wiemann
+
+Kym Pagel
+
+Rick Kym
+
+Olga Papadodima
---
+++ Scoring.SNVs.R
@@ -0,0 +1,150 @@
+# Custom scoring function for variant prioritization based on OpenCRAVAT integrative annotation
+
+
+scoring.func <- function(rdata_dir,exp.genes=NULL){
+
+  
+  # rdata_dir is the full path directory where the relative RData file has been stored
+  # exp.genes is a character vector of expressed genes IDs as HUGO gene symbols
+  # Load the necessary packages
+  
+  
+  require(tidyverse)
+  require(jsonlite)
+  require(DT)
+  require(htmlwidgets)
+  
+ifr <- list.files(path=rdata_dir, ".RData")
+      
+OC.input <-get(load(ifr)) 
+    
+  
+  stopifnot(inherits(OC.input,"data.frame"))
+  stopifnot(ncol(OC.input)>1)
+  
+  if(!any(colnames(OC.input)%in%c("hugo","so","spliceai__ds_dl","spliceai__ds_al", "vest__score",
+                             "fathmm_xf_coding__fathmm_xf_coding_pred","mutation_assessor__impact","siphy__logodds_rank",
+                             "phastcons__phastcons30_mamm_r,cgc__class","cosmic__variant_count,cgl__class",
+                             "cgl__class", "Cancer_hotspots_vcount", "civic__clinical_a_score,clinvar__sig"))) {
+    stop("Not necessary OpenCRAVAT annotator names for creating the merged score !!")
+  }  
+
+  if (is.null(exp.genes)){
+    warning(paste0("No vector of gene symbols is provided, thus expression score would be 0"))
+    
+    d2 <- OC.input %>% mutate(Exp_score=0)
+    
+  } else if (is.character(exp.genes)){
+    
+    d2 <- OC.input %>% mutate(Exp_score= 
+    if_else(hugo%in%c(exp.genes), 1, 0, missing = 0))
+    
+  } 
+  
+  # modify the output of the cancer hotspots annotation db in order to sum the total variant count number
+  
+  ch_dt <- d2 %>%
+    dplyr::select(cancer_hotspots__samples) %>%
+    rownames_to_column() %>%
+    group_by(rowname ) %>%
+    mutate(cancer__hotspots__vc = ifelse(cancer_hotspots__samples == "[]",
+                                         yes = "[{\"tissue\": \"none\", \"count\": 0}]",
+                                         no = cancer_hotspots__samples ) %>%
+             jsonlite::fromJSON(txt = .) %>%
+             summarize(Count=sum(count)) %>%
+             deframe())
+  
+  
+  d2 <- d2 %>% mutate(Cancer_hotspots_vcount=ch_dt$cancer__hotspots__vc)
+  
+  # Initial creation of pathogenicity/effect prediction score
+  
+  d3 <- d2 %>% filter(so %in% c("start_lost","stop_lost", "stop_gained", "missense_variant",
+                                "splice_site_variant")) %>%
+    
+    
+    dplyr::select(hugo,so,spliceai__ds_dl,spliceai__ds_al, vest__score,Exp_score,gnomad__af,
+                  fathmm_xf_coding__fathmm_xf_coding_pred,mutation_assessor__impact,siphy__logodds_rank,
+                  phastcons__phastcons30_mamm_r,cgc__class,cosmic__variant_count,cgl__class,
+                  cgl__class, Cancer_hotspots_vcount, civic__clinical_a_score,clinvar__sig) %>%
+    
+    
+    dplyr::filter((!gnomad__af > 0.1) %>% replace_na(TRUE)) %>% 
+    
+    mutate(Pathogenicity_score= case_when(
+      so=="splice_site_variant" &  (spliceai__ds_dl > 0.5 | spliceai__ds_al > 0.5) ~1,
+      TRUE~0),
+      
+      Pathogenicity2_score=case_when(
+        
+        so %in% c("missense_variant","stop_gained") & vest__score>0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & mutation_assessor__impact %in% c("High","Medium")~1,
+        
+        so %in% c("missense_variant","stop_gained") & vest__score >0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" ~0.8,
+        
+        so %in% c("missense_variant","stop_gained") & vest__score >0.6 & mutation_assessor__impact %in% c("High","Medium") ~0.8,
+        
+        so %in% c("missense_variant","stop_gained") &fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & mutation_assessor__impact %in% c("High","Medium")~0.8,
+        
+        so %in% c("missense_variant","stop_gained") & 
+          vest__score>0.6 | fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" | mutation_assessor__impact %in% c("High","Medium")~0.5,
+        
+        TRUE~0),
+      
+      evol_conserv_score= case_when(
+        
+        siphy__logodds_rank > 0.6 & phastcons__phastcons30_mamm_r > 0.6 ~1,
+        
+        siphy__logodds_rank > 0.6 | phastcons__phastcons30_mamm_r > 0.6 ~0.5,
+        
+        TRUE~0),
+      
+    ) %>% mutate(Merged_Pathogenicity_score = rowSums(select(., Pathogenicity_score,Pathogenicity2_score))) %>% 
+    
+    mutate(Total_predict_score=rowMeans(select(., Merged_Pathogenicity_score,evol_conserv_score)))  
+  
+  
+  
+  # Next we create the cancer evidence score
+  
+  d4 <- d3 %>%
+    
+    mutate(
+      cancer_gene_ev_score=case_when(
+        str_detect(cgc__class,"Oncogene|fusion|TSG") & cgl__class%in%c("Oncogene","TSG")~1,
+        
+        str_detect(cgc__class,"Oncogene|fusion|TSG") | cgl__class%in%c("Oncogene","TSG")~0.5,
+        
+        TRUE~0),
+      
+      cancer_freq_score= case_when(
+        
+        cosmic__variant_count > 10 & Cancer_hotspots_vcount > 10 ~1,
+        
+        cosmic__variant_count > 10 | Cancer_hotspots_vcount > 10 ~0.5,
+        TRUE~0),
+      
+      clin_ev_score= case_when(
+        
+        !is.na(civic__clinical_a_score) & clinvar__sig%in%c("Pathogenic","Likely pathogenic")~1,
+        
+        !is.na(civic__clinical_a_score) | clinvar__sig%in%c("Pathogenic","Likely pathogenic")~0.5,
+        TRUE~0)) %>% mutate(Total_Cancer_score = rowSums(select(., cancer_gene_ev_score,
+                                                                cancer_freq_score)))
+  
+  
+  # Final merging of the created sub-scores
+  
+  d.final <- d4 %>% mutate(Final_Score= rowMeans(select(., 
+  Total_predict_score,Total_Cancer_score,clin_ev_score,Exp_score)))
+  
+  write_csv(d.final,file="Finaloutput.Variant.Ranked.OpenCRAVAT.csv")
+  
+  dat.sel.top10 <- d.final %>% arrange(desc(Final_Score)) %>% top_n(10)
+  
+  mytable <- datatable(dat.sel.top10, filter = 'top', options = list(paging = FALSE))
+  
+  saveWidget(mytable, "mytable.html")
+  
+}
+
+
