6963f114c41bfd2e8006aadc63e6f3beb11f1e30
admin <admin@e240-nb19.inet.dkfz-heidelberg.de>
2022-02-17 16:54:36 +0100

    Includes various major updates/fixes: weighted final scoring, correct loading of RData files, addition/removal of database resources/sequence ontology annotations and sample output prefix customization;

--- Scoring.SNVs.R
+++ Scoring.SNVs.R
@@ -1,8 +1,6 @@
-# Custom scoring function for variant prioritization based on OpenCRAVAT integrative annotation
-
-
-scoring.func.snvs <- function(rdata_dir,exp.genes=NULL){
-
+scoring.func.snvs <- function(rdata_dir,exp.genes=NULL,sample.name.output,
+                              w1=0.7,w2=0.8,w3=0.9,w4=1){
+  
   
   # rdata_dir is the full path directory where the relative RData file has been stored
   # exp.genes is a character vector of expressed genes IDs as HUGO gene symbols
@@ -14,21 +12,23 @@ scoring.func.snvs <- function(rdata_dir,exp.genes=NULL){
   require(DT)
   require(htmlwidgets)
   
-ifr <- list.files(path=rdata_dir, ".RData")
-      
-OC.input <-get(load(ifr)) 
-    
+  ifr <- list.files(path=rdata_dir, ".RData",full.names = T)
+  
+  OC.input <-get(load(ifr)) 
+  
   
   stopifnot(inherits(OC.input,"data.frame"))
   stopifnot(ncol(OC.input)>1)
+  stopifnot(!is.null(sample.name.output))
   
-  if(!any(colnames(OC.input)%in%c("hugo","so","spliceai__ds_dl","spliceai__ds_al", "vest__score",
-                             "fathmm_xf_coding__fathmm_xf_coding_pred","mutation_assessor__impact","siphy__logodds_rank",
-                             "phastcons__phastcons30_mamm_r,cgc__class","cosmic__variant_count,cgl__class",
-                             "cgl__class", "Cancer_hotspots_vcount", "civic__clinical_a_score,clinvar__sig"))) {
+  if(!any(colnames(OC.input)%in%c("chrom", "pos", "ref_base", "alt_base","cchange", 
+                                  "all_mappings","achange", "hugo","transcript","so","samples","numsample","spliceai__ds_dl",
+                                  "vest__score","gnomad__af","fathmm_xf_coding__fathmm_xf_coding_pred","siphy__logodds_rank",   "cscape_coding__score","phastcons__phastcons30_mamm_r","cgc__class",
+                                  "cosmic__variant_count","cgl__class","Cancer_hotspots_vcount", "civic__clinical_a_score","clinvar__sig"))) {
+    
     stop("Not necessary OpenCRAVAT annotator names for creating the merged score !!")
   }  
-
+  
   if (is.null(exp.genes)){
     warning(paste0("No vector of gene symbols is provided, thus expression score would be 0"))
     
@@ -37,7 +37,7 @@ OC.input <-get(load(ifr))
   } else if (is.character(exp.genes)){
     
     d2 <- OC.input %>% mutate(Exp_score= 
-    if_else(hugo%in%c(exp.genes), 1, 0, missing = 0))
+                                if_else(hugo%in%c(exp.genes), 1, 0, missing = 0))
     
   } 
   
@@ -63,13 +63,16 @@ OC.input <-get(load(ifr))
                                 "splice_site_variant")) %>%
     
     
-    dplyr::select(hugo,so,spliceai__ds_dl,spliceai__ds_al, vest__score,Exp_score,gnomad__af,
-                  fathmm_xf_coding__fathmm_xf_coding_pred,mutation_assessor__impact,siphy__logodds_rank,
-                  phastcons__phastcons30_mamm_r,cgc__class,cosmic__variant_count,cgl__class,
-                  cgl__class, Cancer_hotspots_vcount, civic__clinical_a_score,clinvar__sig) %>%
+    dplyr::select(chrom, pos, ref_base, alt_base, hugo,transcript, so, samples,
+                  numsample, cchange, achange,
+                  all_mappings, spliceai__ds_dl, spliceai__ds_al, hg19__chrom, hg19__pos,
+                  vest__score,Exp_score,gnomad__af,fathmm_xf_coding__fathmm_xf_coding_pred,
+                  siphy__logodds_rank, phastcons__phastcons30_mamm_r,cscape_coding__score,
+                  cgc__class,cosmic__variant_count,cgl__class,cgl__class, Cancer_hotspots_vcount, 
+                  civic__clinical_a_score,clinvar__sig) %>%
     
     
-    dplyr::filter((!gnomad__af > 0.1) %>% replace_na(TRUE)) %>% 
+    dplyr::filter((!gnomad__af > 0.05) %>% replace_na(TRUE)) %>% 
     
     mutate(Pathogenicity_score= case_when(
       so=="splice_site_variant" &  (spliceai__ds_dl > 0.5 | spliceai__ds_al > 0.5) ~1,
@@ -77,16 +80,16 @@ OC.input <-get(load(ifr))
       
       Pathogenicity2_score=case_when(
         
-        so %in% c("missense_variant","stop_gained") & vest__score>0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & mutation_assessor__impact %in% c("High","Medium")~1,
+        so %in% c("missense_variant","stop_gained","start_lost","stop_lost") & vest__score>0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & cscape_coding__score>0.6 ~1,
         
-        so %in% c("missense_variant","stop_gained") & vest__score >0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" ~0.8,
+        so %in% c("missense_variant","stop_gained","start_lost","stop_lost") & vest__score >0.6 & fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" ~0.8,
         
-        so %in% c("missense_variant","stop_gained") & vest__score >0.6 & mutation_assessor__impact %in% c("High","Medium") ~0.8,
+        so %in% c("missense_variant","stop_gained","start_lost","stop_lost") & vest__score >0.6 & cscape_coding__score>0.6 ~0.8,
         
-        so %in% c("missense_variant","stop_gained") &fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & mutation_assessor__impact %in% c("High","Medium")~0.8,
+        so %in% c("missense_variant","stop_gained","start_lost","stop_lost") &fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" & cscape_coding__score>0.6 ~0.8,
         
-        so %in% c("missense_variant","stop_gained") & 
-          vest__score>0.6 | fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" | mutation_assessor__impact %in% c("High","Medium")~0.5,
+        so %in% c("missense_variant","stop_gained","start_lost","stop_lost") & 
+          vest__score>0.6 | fathmm_xf_coding__fathmm_xf_coding_pred=="Damaging" | cscape_coding__score>0.6 ~0.5,
         
         TRUE~0),
       
@@ -104,7 +107,7 @@ OC.input <-get(load(ifr))
   
   
   
-  # Next we create the cancer evidence score
+  # Next we create the cancer evidence & the clinical scores
   
   d4 <- d3 %>%
     
@@ -125,26 +128,33 @@ OC.input <-get(load(ifr))
       
       clin_ev_score= case_when(
         
-        !is.na(civic__clinical_a_score) & clinvar__sig%in%c("Pathogenic","Likely pathogenic")~1,
+        !is.na(civic__clinical_a_score) & str_detect(clinvar__sig, "Pathogenic|drug response|pathogenic$")~1,
         
-        !is.na(civic__clinical_a_score) | clinvar__sig%in%c("Pathogenic","Likely pathogenic")~0.5,
-        TRUE~0)) %>% mutate(Total_Cancer_score = rowSums(select(., cancer_gene_ev_score,
-                                                                cancer_freq_score)))
+        !is.na(civic__clinical_a_score) | str_detect(clinvar__sig, "Pathogenic|drug response|pathogenic$")~0.5,
+        TRUE~0)) %>% mutate(Total_Cancer_score = rowMeans(select(., cancer_gene_ev_score,
+                                                                 cancer_freq_score)))
   
   
   # Final merging of the created sub-scores
   
-  d.final <- d4 %>% mutate(Final_Score= rowMeans(select(., 
-  Total_predict_score,Total_Cancer_score,clin_ev_score,Exp_score)))
+  weight.func <- function(a,b,c,d,w1=0.7,w2=0.8,w3=0.9,w4=1){
+    weighted.score <- (a*w1 + b*w2 + c*w3 + d*w4)/3.4
+    return(weighted.score)
+  }
   
-  write_csv(d.final,file="Finaloutput.Variant.Ranked.OpenCRAVAT.csv")
+  
+  d.final <- d4 %>% mutate(Final_Score= weight.func(a=Total_predict_score,b=Total_Cancer_score,
+                                                    c=clin_ev_score,d=Exp_score))
+  
+  
+  write_csv(d.final,file=str_c("Finaloutput.Variant.Ranked.OpenCRAVAT",sample.name.output,"csv",
+                               sep="."))
   
   dat.sel.top10 <- d.final %>% arrange(desc(Final_Score)) %>% top_n(10)
   
   mytable <- datatable(dat.sel.top10, filter = 'top', options = list(paging = FALSE))
   
-  saveWidget(mytable, "mytable.html")
+  saveWidget(mytable, file=str_c("Top10.Ranked.OC",sample.name.output,
+                                 "mytable.html",sep="."))
   
 }
-
-
